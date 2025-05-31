b7f6499f926ff6828b5cf6d2d789499b6f8e9b9d
isaacrochaoliveira <isaacvroliveira@gmail.com>
2025-05-31 20:28:53 -0300

    Prova

---
+++ script.js
@@ -0,0 +1,23 @@
+function resps() {
+    var r1 = document.getElementById('r1').value;
+    var r2 = document.getElementById('r1').value;
+    var r3 = document.getElementById('r1').value;
+    var r4 = document.getElementById('r1').value;
+    var r5 = document.getElementById('r1').value;
+    var r6 = document.getElementById('r1').value;
+    var r7 = document.getElementById('r1').value;
+    var r8 = document.getElementById('r1').value;
+    var r9 = document.getElementById('r1').value;
+    var r10 = document.getElementById('r1').value;
+    var r11 = document.getElementById('r1').value;
+    var r12 = document.getElementById('r1').value;
+    var r13 = document.getElementById('r1').value;
+    var r14 = document.getElementById('r1').value;
+    var r15 = document.getElementById('r1').value;
+
+    for (var c = 1; c < 11; c++) {
+        if (('r' + c) != null) {
+            alert('A Questão ' + c + ' Não pode ficar vazia!');
+        }
+    }
+}
