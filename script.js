dbe5271d71d0c08729c214534f1e7c07efe64343
isaacrochaoliveira <isaacvroliveira@gmail.com>
2025-06-01 11:05:51 -0300

    Prova

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 function resps() {
-    document.getElementsByClassName('spinner').innerHTML('<i class="fa-solid fa-spinner fa-spin-pulse"></i>')
     for (var c = 1; c < 11; c++) {
         if (document.getElementById('r' + c).value == "") {
             alert('A Questão ' + c + ' Não pode ficar vazia!');
         } else {
+            if (c == 1) {
+                if (document.getElementById('r' + c).value == "C") {
+                    alert('Você acertou a primeira pergunta!');
+                }
+            }
         }
     }
 }   
