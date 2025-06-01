118a959ebb13502e739de60c3f55a4391ac3ca12
isaacrochaoliveira <isaacvroliveira@gmail.com>
2025-05-31 21:03:38 -0300

    Prova

--- script.js
+++ script.js
@@ -1,23 +1,9 @@
 function resps() {
-    var r1 = document.getElementById('r1').value;
-    var r2 = document.getElementById('r1').value;
-    var r3 = document.getElementById('r1').value;
-    var r4 = document.getElementById('r1').value;
-    var r5 = document.getElementById('r1').value;
-    var r6 = document.getElementById('r1').value;
-    var r7 = document.getElementById('r1').value;
-    var r8 = document.getElementById('r1').value;
-    var r9 = document.getElementById('r1').value;
-    var r10 = document.getElementById('r1').value;
-    var r11 = document.getElementById('r1').value;
-    var r12 = document.getElementById('r1').value;
-    var r13 = document.getElementById('r1').value;
-    var r14 = document.getElementById('r1').value;
-    var r15 = document.getElementById('r1').value;
-
+    document.getElementsByClassName('spinner').innerHTML('<i class="fa-solid fa-spinner fa-spin-pulse"></i>')
     for (var c = 1; c < 11; c++) {
-        if (('r' + c) != null) {
+        if (document.getElementById('r' + c).value == "") {
             alert('A Questão ' + c + ' Não pode ficar vazia!');
+        } else {
         }
     }
-}
+}   
